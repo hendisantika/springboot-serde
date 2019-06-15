d36ab611b3d462f5a6e440744d2887f23734ff7e
hendisantika <hendisantika@yahoo.co.id>
2019-06-16 05:59:53 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootserde/SpringbootSerdeApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootserde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSerdeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSerdeApplication.class, args);
+    }
+
+}
