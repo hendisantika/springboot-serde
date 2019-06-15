82f13345e2a3a15aec605688015be141c6f09c51
hendisantika <hendisantika@yahoo.co.id>
2019-06-16 06:03:04 +0700

    #4 - Add Animal Model

---
+++ src/main/java/com/hendisantika/springbootserde/model/Animal.java
@@ -0,0 +1,35 @@
+package com.hendisantika.springbootserde.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-serde
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-06-16
+ * Time: 06:02
+ */
+public class Animal implements Serializable {
+    private static final long serialVersionUID = -8796619172458378159L;
+    private String name;
+    private String type;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+}
